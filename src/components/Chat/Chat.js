f7ac5b85094d1e8ea7591de824ac6540b6843608
unknown <killedsparrow@gmail.com>
2024-08-28 13:36:02 +0400

    conflict resolved

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -35,11 +35,7 @@ const Chat = () => {
         const fetchContacts = async () => {
             try {
                 const response = await axios.get('https://api-rubin.multfilm.tatar/api/messages/contacts', {
-<<<<<<< HEAD
                     headers: { 'Authorization': `Bearer ${token}` }
-=======
-                    headers: { Authorization: `${token}` }
->>>>>>> origin/anauthorized-access
                 });
                 setContacts(response.data.data.map(contact => ({ ...contact, unreadCount: 0 })));
             } catch (error) {
