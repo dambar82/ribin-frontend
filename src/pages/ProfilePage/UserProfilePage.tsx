3e90ad4df105f54accffd5da5c0dbba5e896fe33
HonestDaniel <killedsparrow@gmail.com>
2024-09-06 15:55:14 +0400

    sms

--- src/pages/ProfilePage/UserProfilePage.tsx
+++ src/pages/ProfilePage/UserProfilePage.tsx
@@ -82,7 +82,7 @@ const UserProfilePage = () => {
                                     </div>
                                     ) : (
                                     <div className="big-card__actions">
-                                        <Link to={`/chat/${id}`}>
+                                        <Link to={`/chat/${currentUser.id}`}>
                                         <button className="button button--main-outlined" type="button">
                                             <span>Написать сообщение</span>
                                         </button>
