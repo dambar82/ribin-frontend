44023ad2aaabe4016bba707aa0ab5d2a25bde818
unknown <killedsparrow@gmail.com>
2024-09-10 10:11:54 +0400

    bug resolved

--- src/shared/utils/getMostActiveUsers/getMostActiveUsers.ts
+++ src/shared/utils/getMostActiveUsers/getMostActiveUsers.ts
@@ -2,10 +2,12 @@ import { User } from "../../../store/userSlice"
 
 export const getMostActiveUsers = ( users: User[] | undefined, quantity: number ) => {
 
-  //@ts-ignore
-  const sorted = users?.toSorted((a, b) => a.rubick - b.rubick)
+  if (users) {
+    //@ts-ignore
+    const sorted = users?.toSorted((a, b) => a.rubick - b.rubick)
 
-  if ( !sorted ) return []
+    if ( !sorted ) return []
 
-  return sorted.slice((-1)*quantity).reverse()
+    return sorted.slice((-1)*quantity).reverse()
+  }
 }
