c896e580257a451c4695fd69752a0b9f743897e1
unknown <killedsparrow@gmail.com>
2024-09-23 17:06:43 +0400

    upd

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -39,7 +39,7 @@ const Grid = ({ children, totalItems, itemsPerPage = 6 }: GridProps) => {
 
     return (
         <div className='gird'>
-            <div className='grid__list'>
+            <div className='grid__list' style={{backgroundColor: '#F8F8F8', padding: '70px', borderRadius: '35px'}}>
                 {childrenArray.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)}
             </div>
             { totalPages > 1 && (
