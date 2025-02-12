4a3f94e75dc3d27f2453d088a4242f60fffb1906
unknown <killedsparrow@gmail.com>
2025-02-12 15:55:17 +0400

    scroll

--- src/pages/AchievementsPage/AchievementsPage.tsx
+++ src/pages/AchievementsPage/AchievementsPage.tsx
@@ -179,6 +179,39 @@ const AchievementsPage = () => {
                                         </div>
                                     )
                                 }
+                                {/*<div className={styles.promo}>*/}
+                                {/*    <div className={styles.promo_logo}>*/}
+                                {/*        <img src="/images/rubin%20logo.png" alt=""/>*/}
+                                {/*    </div>*/}
+                                {/*    <div className={styles.promo_info}>*/}
+                                {/*        <h2>Билеты на матчи ФК «Рубин» - ФК «Спартак» и ФК «Рубин» - ФК «Пари НН»</h2>*/}
+                                {/*        /!*<div className={styles.promo_info_button}>*!/*/}
+                                {/*        /!*    <span>50%</span> на все сектора*!/*/}
+                                {/*        /!*</div>*!/*/}
+                                {/*    </div>*/}
+                                {/*    <div className={styles.promo_code}>*/}
+                                {/*        <div className={`${styles.promo_code_copy} ${isCopied ? styles.promo_code_copy_Copied : ''}`}>*/}
+                                {/*            {*/}
+                                {/*                copiedPromo === 'ДНЕВНИК' && (*/}
+                                {/*                    <div className={styles.promoMessage}>*/}
+                                {/*                        Промокод скопирован*/}
+                                {/*                    </div>*/}
+                                {/*                )*/}
+                                {/*            }*/}
+                                {/*            <p ref={promo}>ДНЕВНИК</p>*/}
+                                {/*            {//@ts-ignore*/}
+                                {/*                <div className={styles.promo_code_copy_round} onClick={() => copyToClipboard(promo.current.innerText)}>*/}
+                                {/*                    <img src={copy} alt=""/>*/}
+                                {/*                </div>*/}
+                                {/*            }*/}
+                                {/*        </div>*/}
+                                {/*        <Link to='https://tickets.rubin-kazan.ru/event?id_event=668' target='_blank'>*/}
+                                {/*            <div className={styles.promo_code_go}>*/}
+                                {/*                <img src={gosomewhere} alt=""/>*/}
+                                {/*            </div>*/}
+                                {/*        </Link>*/}
+                                {/*    </div>*/}
+                                {/*</div>*/}
                             </div>
                             <div className={styles.awards_grid}>
                                 {sortedAwards.map(item => (
--- src/pages/BalanceHistory/BalanceHistory.tsx
+++ src/pages/BalanceHistory/BalanceHistory.tsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from './BalanceHistory.module.scss';
 import rubikStraight from '../../images/svg/rubikStraight.svg';
 import axios from "axios";
 import {useSelector} from "react-redux";
 import {RootState} from "../../store/store";
+import blackArrowUp from "../../images/svg/blackArrowUp.svg";
+import blackArrowDown from "../../images/svg/blackArrowDown.svg";
 
 const token = JSON.parse(localStorage.getItem('token') || '0')
 
@@ -29,26 +31,119 @@ function transformDate(dateStr) {
 
 const BalanceHistory = () => {
 
+    const [selectedOption, setSelectedOption] = useState('Все');
+    const selectRef = useRef(null);
+    const [selectIsOpen, setSelectIsOpen] = useState(false);
+
     const user = useSelector((state: RootState) => state.user.user);
     const [history, setHistory] = useState([]);
+    const [displayedHistory, setDisplayedHistory] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const containerRef = useRef(null);
+    const [hasMore, setHasMore] = useState(true);
+
+    const toggleSelect = () => {
+        setSelectIsOpen(!selectIsOpen);
+    };
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+        setSelectIsOpen(false);
+    };
+
+    const handleClickOutside = (event) => {
+        if (selectRef.current && !selectRef.current.contains(event.target)) {
+            setSelectIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     useEffect(() => {
         const fetchHistory = async () => {
-            const response = await axios.get('https://api-rubin.multfilm.tatar/api/rubicks', {
-                headers: { 'Authorization': `Bearer ${token}` }
-            })
-            console.log(response.data)
-            setHistory(response.data.data)
-        }
+            try {
+                const response = await axios.get(
+                    "https://api-rubin.multfilm.tatar/api/rubicks",
+                    {
+                        headers: { Authorization: `Bearer ${token}` },
+                    }
+                );
+
+                setHistory(response.data.data);
+                const initialFiltered = response.data.data.filter((item) => {
+                    if (selectedOption === "Все") return true;
+                    if (selectedOption === "Зачисления") return item.up_rubicks !== null;
+                    if (selectedOption === "Списания") return item.up_rubicks === null;
+                    return true;
+                });
+
+                setDisplayedHistory(initialFiltered.slice(0, 20)); // Показываем первые 20 записей
+                setHasMore(initialFiltered.length > 20); // Проверяем, есть ли еще данные
+            } catch (error) {
+                console.error("Ошибка загрузки данных:", error);
+            }
+        };
 
         fetchHistory();
-    }, [])
+    }, []);
+
+    const filteredHistory = history.filter((item) => {
+        if (selectedOption === "Все") return true;
+        if (selectedOption === "Зачисления") return item.up_rubicks !== null;
+        if (selectedOption === "Списания") return item.up_rubicks === null;
+        return true;
+    });
+
+
+    // Загружает новые элементы при скролле вниз
+    const loadMoreItems = () => {
+        if (loading || !hasMore) return; // Если уже загружаем, ничего не делаем
+        setLoading(true);
+
+        setTimeout(() => {
+            const newPage = page + 1;
+            const newItems = filteredHistory.slice(0, newPage * 20); // По 10 элементов за раз
+            setDisplayedHistory(newItems);
+            setPage(newPage);
+            setLoading(false);
+
+            if (newItems.length >= filteredHistory.length) {
+                setHasMore(false); // Если все данные загружены, больше не загружаем
+            }
+
+        }, 1000);
+    };
+
+    // Отслеживаем скролл
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting && hasMore) {
+                    loadMoreItems();
+                }
+            },
+            { threshold: 1.0 }
+        );
+
+        if (containerRef.current) {
+            observer.observe(containerRef.current);
+        }
+
+        return () => observer.disconnect();
+    }, [loading, hasMore]);
 
     useEffect(() => {
-        console.log(history)
-    }, [history])
+        setDisplayedHistory(filteredHistory.slice(0, 20));
+        setPage(1);
+        setHasMore(filteredHistory.length > 20); // Пересчитываем hasMore
+    }, [selectedOption]);
 
-    // @ts-ignore
     return (
         <div className='page'>
             <section className={`${styles.balanceHistory} section`}>
@@ -62,12 +157,27 @@ const BalanceHistory = () => {
                 <div className={styles.main}>
                     <div className={styles.main_header}>
                         <h3>Условие</h3>
+                        <div className={styles.awards_select} ref={selectRef}>
+                            <div className={styles.awards_selected} onClick={toggleSelect}>
+                                <span>{selectedOption}</span>
+                                {selectIsOpen ? <img src={blackArrowUp} alt=''/> : <img src={blackArrowDown} alt=''/>}
+                            </div>
+                            {selectIsOpen && (
+                                <div className={styles.awards_selectItems}>
+                                    {['Все', 'Зачисления', 'Списания'].map((option, index) => (
+                                        <div key={index} onClick={() => handleOptionClick(option)}>
+                                            {option}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
                     </div>
                     {
-                        history && history.length > 0 && (
+                        displayedHistory.length > 0 && (
                         <div className={styles.main_flex}>
                             {
-                                history.map(item => (
+                                displayedHistory.map(item => (
                                     <div className={styles.item}>
                                         <div className={styles.item_part}>
                                             <span className={styles.date}>{transformDate(item.date)}</span>
@@ -100,6 +210,9 @@ const BalanceHistory = () => {
                             }
                         </div>
                     )}
+                    <div ref={containerRef} style={{ height: '20px' }}></div>
+
+                    {loading && <p>Загружаем ещё...</p>}
                 </div>
             </section>
         </div>
